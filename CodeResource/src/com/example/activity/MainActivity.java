3d3bb91cb243496bc11c1ad40699ba294f3eff94
chenlang <anikaka@sina.com>
2014-12-16 09:58:33 +0800

    commit

---
+++ CodeResource/src/com/example/activity/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.activity;
+
+import com.example.coderesource.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
