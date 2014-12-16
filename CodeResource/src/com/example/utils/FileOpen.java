8e30231079a1a15a6677cc055bc7b55c8df7e498
chenlang <anikaka@sina.com>
2014-12-16 09:00:01 +0800

    commit

---
+++ CodeResource/src/com/example/utils/FileOpen.java
@@ -0,0 +1,148 @@
+package com.example.utils;
+import java.io.File;
+
+import android.content.Intent;
+import android.net.Uri;
+
+
+public class FileOpen {
+
+	
+	   //Android鑾峰彇涓�釜鐢ㄤ簬鎵撳紑APK鏂囦欢鐨刬ntent  
+    public static Intent getAllIntent( String param ) {  
+  
+        Intent intent = new Intent();    
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);    
+        intent.setAction(android.content.Intent.ACTION_VIEW);    
+        Uri uri = Uri.fromFile(new File(param ));  
+        intent.setDataAndType(uri,"*/*");   
+        return intent;  
+    }  
+    //Android鑾峰彇涓�釜鐢ㄤ簬鎵撳紑APK鏂囦欢鐨刬ntent  
+    public static Intent getApkFileIntent(File file ) {  
+  
+        Intent intent = new Intent();    
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);    
+        intent.setAction(android.content.Intent.ACTION_VIEW);    
+        Uri uri = Uri.fromFile(file);  
+        intent.setDataAndType(uri,"application/vnd.android.package-archive");   
+        return intent;  
+    }  
+  
+    //Android鑾峰彇涓�釜鐢ㄤ簬鎵撳紑VIDEO鏂囦欢鐨刬ntent  
+    public static Intent getVideoFileIntent( String param ) {  
+  
+        Intent intent = new Intent("android.intent.action.VIEW");  
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);  
+        intent.putExtra("oneshot", 0);  
+        intent.putExtra("configchange", 0);  
+        Uri uri = Uri.fromFile(new File(param ));  
+        intent.setDataAndType(uri, "video/*");  
+        return intent;  
+    }  
+  
+    //Android鑾峰彇涓�釜鐢ㄤ簬鎵撳紑AUDIO鏂囦欢鐨刬ntent  
+    public static Intent getAudioFileIntent( String param ){  
+  
+        Intent intent = new Intent("android.intent.action.VIEW");  
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);  
+        intent.putExtra("oneshot", 0);  
+        intent.putExtra("configchange", 0);  
+        Uri uri = Uri.fromFile(new File(param ));  
+        intent.setDataAndType(uri, "audio/*");  
+        return intent;  
+    }  
+  
+    //Android鑾峰彇涓�釜鐢ㄤ簬鎵撳紑Html鏂囦欢鐨刬ntent     
+    public static Intent getHtmlFileIntent( String param ){  
+  
+        Uri uri = Uri.parse(param ).buildUpon().encodedAuthority("com.android.htmlfileprovider").scheme("content").encodedPath(param ).build();  
+        Intent intent = new Intent("android.intent.action.VIEW");  
+        intent.setDataAndType(uri, "text/html");  
+        return intent;  
+    }  
+  
+    //Android鑾峰彇涓�釜鐢ㄤ簬鎵撳紑鍥剧墖鏂囦欢鐨刬ntent  
+    public static Intent getImageFileIntent(File file ) {  
+  
+        Intent intent = new Intent("android.intent.action.VIEW");  
+        intent.addCategory("android.intent.category.DEFAULT");  
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);  
+        Uri uri = Uri.fromFile(file);  
+        intent.setDataAndType(uri, "image/*");  
+        return intent;  
+    }  
+  
+    //Android鑾峰彇涓�釜鐢ㄤ簬鎵撳紑PPT鏂囦欢鐨刬ntent     
+    public static Intent getPptFileIntent( String param ){    
+  
+        Intent intent = new Intent("android.intent.action.VIEW");     
+        intent.addCategory("android.intent.category.DEFAULT");     
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);     
+        Uri uri = Uri.fromFile(new File(param ));     
+        intent.setDataAndType(uri, "application/vnd.ms-powerpoint");     
+        return intent;     
+    }     
+  
+    //Android鑾峰彇涓�釜鐢ㄤ簬鎵撳紑Excel鏂囦欢鐨刬ntent     
+    public static Intent getExcelFileIntent(File file){    
+  
+        Intent intent = new Intent("android.intent.action.VIEW");     
+        intent.addCategory("android.intent.category.DEFAULT");     
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);     
+        Uri uri = Uri.fromFile(file);     
+        intent.setDataAndType(uri, "application/vnd.ms-excel");     
+        return intent;     
+    }     
+  
+    //Android鑾峰彇涓�釜鐢ㄤ簬鎵撳紑Word鏂囦欢鐨刬ntent     
+    public static Intent getWordFileIntent(File file){    
+  
+        Intent intent = new Intent("android.intent.action.VIEW");     
+        intent.addCategory("android.intent.category.DEFAULT");     
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);     
+        Uri uri = Uri.fromFile(file);     
+        intent.setDataAndType(uri,"application/msword");//    
+        return intent;     
+    }     
+  
+    //Android鑾峰彇涓�釜鐢ㄤ簬鎵撳紑CHM鏂囦欢鐨刬ntent     
+    public static Intent getChmFileIntent( String param ){     
+  
+        Intent intent = new Intent("android.intent.action.VIEW");     
+        intent.addCategory("android.intent.category.DEFAULT");     
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);     
+        Uri uri = Uri.fromFile(new File(param ));     
+        intent.setDataAndType(uri, "application/x-chm");     
+        return intent;     
+    }     
+  
+    //Android鑾峰彇涓�釜鐢ㄤ簬鎵撳紑鏂囨湰鏂囦欢鐨刬ntent     
+    public static Intent getTextFileIntent( String param, boolean paramBoolean){     
+  
+        Intent intent = new Intent("android.intent.action.VIEW");     
+        intent.addCategory("android.intent.category.DEFAULT");     
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);     
+        if (paramBoolean){     
+            Uri uri1 = Uri.parse(param );     
+            intent.setDataAndType(uri1, "text/plain");     
+        }else{     
+            Uri uri2 = Uri.fromFile(new File(param ));     
+            intent.setDataAndType(uri2, "text/plain");     
+        }     
+        return intent;     
+    }    
+    
+    //Android鑾峰彇涓�釜鐢ㄤ簬鎵撳紑PDF鏂囦欢鐨刬ntent     
+    public static Intent getPdfFileIntent(File file){     
+  
+        Intent intent = new Intent("android.intent.action.VIEW");     
+        intent.addCategory("android.intent.category.DEFAULT");     
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);     
+        Uri uri = Uri.fromFile(file);     
+        intent.setDataAndType(uri, "application/pdf");     
+        return intent;     
+    }  
+    
+    
+}
